e1245dec42f40622ee07b1d87eebdded5ac4713a
Francois-Louis <fl.toussaint.art@gmail.com>
2022-08-12 21:27:11 +0200

    motion for footer and mouse

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -7,8 +7,37 @@ import Profile from '../Profile/profile';
 import Error from '../Error/error';
 import Gallery from "../Gallery/gallery";
 import * as React from 'react';
+import {useEffect} from "react";
+import gsap from "gsap";
 
 const App = () => {
+    useEffect(() => {
+        gsap.set(".ball", {xPercent: -50, yPercent: -50});
+
+        const ball = document.querySelector(".ball");
+        const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+        const mouse = { x: pos.x, y: pos.y };
+        const speed = 0.35;
+
+        const xSet = gsap.quickSetter(ball, "x", "px");
+        const ySet = gsap.quickSetter(ball, "y", "px");
+
+        window.addEventListener("mousemove", e => {
+            mouse.x = e.x;
+            mouse.y = e.y;
+        });
+
+        gsap.ticker.add(() => {
+
+            // adjust speed for higher refresh monitors
+            const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+            pos.x += (mouse.x - pos.x) * dt;
+            pos.y += (mouse.y - pos.y) * dt;
+            xSet(pos.x);
+            ySet(pos.y);
+        });
+    }, );
     return (
       <div className="app">
         <Header />
@@ -21,6 +50,7 @@ const App = () => {
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
+          <div className="ball"></div>
       </div>
   );
 }
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,21 +1,43 @@
 import './footer.scss';
 import gsap from "gsap";
+import { useEffect, useRef} from "react";
 
 const Footer = () => {
   const onEnter = ({ currentTarget }) => {
     gsap.to(currentTarget, { color: "#511730",scale: 1.2 });
   };
-
   const onLeave = ({ currentTarget }) => {
     gsap.to(currentTarget, { color: "#DD625B",scale: 1 });
   };
+  const motion = gsap.timeline();
+  const mail = useRef(null);
+  const git = useRef(null);
+  const lkdn = useRef(null);
+
+  useEffect(() => {
+    motion.from(mail.current, {
+      duration: .5,
+      x: +100,
+      opacity: 0
+    },"1.6")
+    motion.from(git.current, {
+      duration: .5,
+      x: +100,
+      opacity: 0
+    },"1.7")
+    motion.from(lkdn.current, {
+      duration: .5,
+      x: +100,
+      opacity: 0
+    },"1.8")
+  },);
 
     return (
         <footer id="footer">
           <ul>
-            <li onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="mailto://fl.toussaint@protonmail.com" >email</a></li>
-            <li onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="https://github.com/Francois-Louis">github</a></li>
-            <li onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="https://www.linkedin.com/in/françois-louis-t">linkedin</a></li>
+            <li ref={mail} onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="mailto://fl.toussaint@protonmail.com" >email</a></li>
+            <li ref={git} onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="https://github.com/Francois-Louis">github</a></li>
+            <li ref={lkdn} onMouseEnter={onEnter} onMouseLeave={onLeave}><a href="https://www.linkedin.com/in/françois-louis-t">linkedin</a></li>
           </ul>
         </footer>
     );
